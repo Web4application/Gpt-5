3f959515195b94ce6b942cab39aa36e899ab2068
Web4 <167559384+Web4application@users.noreply.github.com>
2025-09-01 06:55:32 +0100

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,7 +1,7 @@
 // import OpenAI from "openai";
 
 const client = new OpenAI({
-  apiKey: process.env.AIzaSyAvrxOyAVzPVcnzxuD0mjKVDyS2bNWfC10
+  apiKey: process.env.OPENAI_API_KEY
 });
 
 export default async function handler(req, res) {
