7fd63048e379c498e9899649966918823ac095a4
Web4 <167559384+Web4application@users.noreply.github.com>
2025-09-25 13:16:22 +0100

    Update prompt.js

--- src/prompt.js
+++ src/prompt.js
@@ -3,12 +3,34 @@ from openai import OpenAI
 
 token = os.environ["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"]
 endpoint = "https://models.github.ai/inference"
-model = "openai/gpt-5"
+model = "gpt-5"
 
 client = OpenAI(
-    base_url=endpoint,
-    api_key=token,
+  base_url = "https://integrate.api.nvidia.com/v1",
+  api_key = "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"
 )
+
+completion = client.chat.completions.create(
+  model="nvidia/nvidia-nemotron-nano-9b-v2",
+  messages=[{"role":"user","content":""}],
+  temperature=0.6,
+  top_p=0.95,
+  max_tokens=2048,
+  frequency_penalty=0,
+  presence_penalty=0,
+  stream=False,
+  extra_body={
+    "min_thinking_tokens": 1024,
+    "max_thinking_tokens": 2048
+  }
+)
+
+reasoning = getattr(completion.choices[0].message, "reasoning_content", None)
+if reasoning:
+  print(reasoning)
+print(completion.choices[0].message.content)
+
+
 
 response = client.chat.completions.create(
     messages=[
