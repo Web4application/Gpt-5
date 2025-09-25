6b12d7bda9489357e27198a402670da5e69a010a
Web4 <167559384+Web4application@users.noreply.github.com>
2025-09-25 14:59:31 +0100

    Update skeleton.tsx

--- components/ui/skeleton.tsx
+++ components/ui/skeleton.tsx
@@ -1,9 +1,9 @@
-import { cn } from "@/lib/utils"
+import { Skeleton } from "@/components/ui/skeleton"
 
 function Skeleton({ className, ...props }: React.ComponentProps<"div">) {
   return (
     <div
-      data-slot="skeleton"
+      data-slot="<Skeleton className="h-[20px] w-[100px] rounded-full" />"
       className={cn("bg-accent animate-pulse rounded-md", className)}
       {...props}
     />
